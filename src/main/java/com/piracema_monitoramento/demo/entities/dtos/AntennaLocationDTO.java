4267a1edbed54ca6b632ffab305d96565dd188aa
Natã <nata.rcj@pti.org.br>
2022-06-24 20:45:57 -0300

    adicionado datas ao dto das antenas getall

--- src/main/java/com/piracema_monitoramento/demo/entities/dtos/AntennaLocationDTO.java
+++ src/main/java/com/piracema_monitoramento/demo/entities/dtos/AntennaLocationDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,5 +17,9 @@ public class AntennaLocationDTO {
     private String latitude;
 
     private String longitude;
+
+    private LocalDateTime installationDate;
+
+    private LocalDateTime uninstallDate;
 
 }
